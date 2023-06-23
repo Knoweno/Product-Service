898f45a85406135736fe1b1b47b271ac4309bf05
Knowen Obiso <knowenemmanuel@gmail.com>
2023-06-23 16:10:38 +0300

    created springboot microservice

--- src/main/java/com/knowenobiso/Product/Service/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knowenobiso.Product.Service.repository;
-
-import com.knowenobiso.Product.Service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product,String>{
-
-
-}
